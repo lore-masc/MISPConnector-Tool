05936b6c50750389204b8c2d61e91aeffbc32642
Lorenzo <lorenki@hotmail.it>
2018-04-27 17:59:11 +0200

    Multiple platforms loaded from online MISP instance

--- src/mispconnector/tool/MISPConnectorTool.java
+++ src/mispconnector/tool/MISPConnectorTool.java
@@ -41,6 +41,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
@@ -50,6 +51,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Pair;
 import javax.net.ssl.HostnameVerifier;
@@ -72,12 +75,12 @@ public class MISPConnectorTool extends Application {
     static final ObservableList<Pair<Integer, String>> groups = FXCollections.observableArrayList(new Pair<Integer, String>(0, "Your organisation only"), new Pair<Integer, String>(1, "This community only"), new Pair<Integer, String>(2, "Connected communities"), new Pair<Integer, String>(3, "All communities"));
     static final ObservableList<String> analysis_options = FXCollections.observableArrayList("Initial", "Ongoing", "Completed");
     static final ObservableList<String> threat_options = FXCollections.observableArrayList("High", "Medium", "Low", "Undefined");
-    static final ObservableList<String> platform_options = FXCollections.observableArrayList("google android", "apple ios", "microsoft windows", "unix unix");
     static final ObservableList<String> tlp_options = FXCollections.observableArrayList("amber", "ex:chr", "green", "red", "white");
+    static final ObservableList<String> platform_options = FXCollections.observableArrayList();
+    static final ObservableList<String> local_platform_options = FXCollections.observableArrayList();
     
     @Override
     public void start(Stage primaryStage) {
-        
         try{
             BufferedReader br = new BufferedReader(new FileReader(CONFIG_FILE));
             url = br.readLine();
@@ -89,11 +92,34 @@ public class MISPConnectorTool extends Application {
         
         //---------------------------- LEFT BAR ----------------------------------
         Label recent_events = new Label("Recents:");
-        ListView list_files = new ListView(files);
-        list_files.setPrefSize(200, 700);
+        ListView<String> list_files = new ListView<String>(files);
         list_files.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         populate_files(list_files);
-        VBox left_bar = new VBox(recent_events, list_files);
+        
+        TextField search_txt = new TextField();
+        search_txt.setPromptText("Search available platforms");
+        search_txt.setPrefWidth(300);
+        HBox search_bar = new HBox(search_txt);
+        search_bar.setAlignment(Pos.CENTER);
+        search_bar.setSpacing(10);
+        
+        ListView<String> list_platform = new ListView<String>(platform_options);
+        list_platform.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        
+        Label platform_lbl = new Label("*Selected platforms: ");
+        Button clear_btn = new Button("X");
+        Button remove_btn = new Button("<<");
+        Button upload_btn = new Button(">>");
+        HBox btn_bar = new HBox(platform_lbl, clear_btn, remove_btn, upload_btn);
+        btn_bar.setAlignment(Pos.CENTER);
+        btn_bar.setSpacing(10);
+        
+        ListView<String> list_local_platform = new ListView<String>(local_platform_options);
+        list_local_platform.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        
+        VBox left_bar = new VBox(recent_events, list_files, search_bar, list_platform, btn_bar, list_local_platform);
+        left_bar.setAlignment(Pos.CENTER);
+        left_bar.setSpacing(10);
         //------------------------------------------------------------------------
         
         //---------------------------- RIGHT BAR ---------------------------------
@@ -146,6 +172,7 @@ public class MISPConnectorTool extends Application {
         //-------------- DESCRIPTION BAR --------------------
         Label description_lbl = new Label("Description: ");
         TextArea description_txt = new TextArea();
+        description_txt.setWrapText(true);
         
         VBox topBar5 = new VBox(description_lbl, description_txt);
         topBar5.setAlignment(Pos.CENTER);
@@ -162,17 +189,13 @@ public class MISPConnectorTool extends Application {
         //---------------------------------------------------
         
         //-------------- TAGs BAR ---------------------------
-        Label platform_lbl = new Label("*Platform: ");
-        ChoiceBox platform_box = new ChoiceBox(platform_options);
-        platform_box.setValue("");
-        
         Label tlp_lbl = new Label("TLP: ");
         ChoiceBox tlp_box = new ChoiceBox(tlp_options);
         tlp_box.setValue(tlp_options.get(0));
         
-        Button submit = new Button("IMPORT");
+        Button submit_btn = new Button("IMPORT");
         
-        HBox top_bar7 = new HBox(platform_lbl, platform_box, tlp_lbl, tlp_box, submit);
+        HBox top_bar7 = new HBox(tlp_lbl, tlp_box, submit_btn);
         top_bar7.setAlignment(Pos.CENTER);
         top_bar7.setSpacing(10);
         //---------------------------------------------------
@@ -182,11 +205,59 @@ public class MISPConnectorTool extends Application {
         content.setSpacing(20);
         content.setPrefSize(600, 700);
         //------------------------------------------------------------------------
+        
+        //---------------------- CLEAR CLICK EVENT -------------------------------
+        clear_btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                local_platform_options.clear();
+            }
+        });
+        //------------------------------------------------------------------------
+        
+        //---------------------- SEARCH CHANGE TEXT EVENT ------------------------
+        search_txt.setOnKeyPressed(new EventHandler<KeyEvent>(){
+            @Override
+            public void handle(KeyEvent ke){
+                if (ke.getCode().equals(KeyCode.ENTER))
+                    if (search_txt.getText().length() > 2){
+                        platform_options.clear();
+                        load_platforms(search_txt.getText());
+                    }else
+                        showErrorDialog("Sono necessari almeno tre caratteri per iniziare la ricerca!", "");
+            }
+        });
+        //------------------------------------------------------------------------
+        
+        //---------------------- REMOVE CLICK EVENT ------------------------------
+        remove_btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                for(int i : list_local_platform.getSelectionModel().getSelectedIndices())
+                    local_platform_options.remove(i);
+            }
+        });
+        //------------------------------------------------------------------------
+        
+        //---------------------- UPLOAD CLICK EVENT ------------------------------
+        upload_btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(list_platform.getSelectionModel().getSelectedItems().isEmpty())
+                    return;
+                
+                for(String platform : list_platform.getSelectionModel().getSelectedItems())
+                    if(!local_platform_options.contains(platform))
+                        local_platform_options.add(platform);
+                list_local_platform.setItems(local_platform_options);
+            }
+        });
+        //------------------------------------------------------------------------
 
-        //---------------------- BUTTON CLICK EVENT ------------------------------
-        submit.setOnAction(new EventHandler<ActionEvent>() {
+        //---------------------- SUBMIT CLICK EVENT ------------------------------
+        submit_btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                if(auth_key_txt.getText().equals("") || title_txt.getText().equals("") || date_picker.getValue().equals("") || platform_box.getValue().equals("")){
+                if(auth_key_txt.getText().equals("") || title_txt.getText().equals("") || date_picker.getValue().equals("") || list_local_platform.getItems().isEmpty()){
                     showErrorDialog("Sono presenti campi obbligatori non compilati!", "");
                     return;
                 }
@@ -221,7 +292,10 @@ public class MISPConnectorTool extends Application {
                     }
                     br.append("],\n");
                     br.append("\t\"platform\": [");
-                    br.append("{\"name\":\"" + platform_box.getValue() + "\"}");
+                    for(String platform : local_platform_options){
+                        br.append("{\"name\":\"" + platform + "\"}");
+                        if(local_platform_options.get(local_platform_options.size() - 1) != platform)  br.append(", ");
+                    }
                     br.append("],\n");
                     br.append("\t\"tlp\": \"" + tlp_box.getValue()+ "\"\n");
                     br.append("}");
@@ -270,7 +344,12 @@ public class MISPConnectorTool extends Application {
                         JSONObject ioc = datajson.getJSONArray("iocs").getJSONObject(i);
                         iocs_txt.setText(iocs_txt.getText() + ((i > 0) ? "\n" : "") + ioc.getString("value"));
                     }
-                    platform_box.setValue(datajson.getJSONArray("platform").getJSONObject(0).getString("name"));
+                    local_platform_options.clear();
+                    for(int i = 0; i < datajson.getJSONArray("platform").length(); i++){
+                        JSONObject platform = datajson.getJSONArray("platform").getJSONObject(i);
+                        local_platform_options.add(platform.getString("name"));
+                    }
+                    list_local_platform.setItems(local_platform_options);
                     tlp_box.setValue(datajson.getString("tlp"));
                 } catch (FileNotFoundException ex) {
                     Logger.getLogger(MISPConnectorTool.class.getName()).log(Level.SEVERE, null, ex);
@@ -296,6 +375,24 @@ public class MISPConnectorTool extends Application {
         try {
             Process pr = rt.exec("update_event.exe " + json);
         } catch (IOException ex) {
+            Logger.getLogger(MISPConnectorTool.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void load_platforms(String search) {
+        try {
+            search = search.replaceAll(" ", "_");
+            search = URLEncoder.encode(search, "UTF-8");
+            HttpsURLConnection conn = MISP_connect("tags/index/searchall:" + search + ".json", "GET");
+            String resp = getResponseConnection(conn);
+            JSONObject datajson = new JSONObject(resp);
+            for(int i = 0; i < datajson.getJSONArray("Tag").length(); i++){
+                String platform = datajson.getJSONArray("Tag").getJSONObject(i).getString("name").split("=")[1];
+                platform_options.add(platform);
+            }
+            conn.disconnect();
+        } catch (Exception ex) {
+            showErrorDialog("Impossibile stabilire la connessione con il server", ex.getMessage());
             Logger.getLogger(MISPConnectorTool.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
